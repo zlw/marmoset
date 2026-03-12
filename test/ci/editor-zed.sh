4806bec2116c61cff4c6f7da21665441845bc7d4
Krzysztof Zalewski <zlw.zalewski@gmail.com>
2026-03-12 15:06:23 +0100

    Make Zed CI tolerate missing ripgrep
    - fall back to grep -Fq when rg is unavailable in the editor-zed environment
    - keep the existing fixed-string assertions unchanged when ripgrep is installed
    
    Testing:
    - ./test/ci/editor-zed.sh
    - ./test/ci/quality.sh
    - make unit

--- test/ci/editor-zed.sh
+++ test/ci/editor-zed.sh
@@ -11,15 +11,25 @@ cargo check --locked
 
 EXPECTED_GRAMMAR_COMMIT="d7e2341c352f8099b5768310bbe24875c9f90c7e"
 
-rg -Fq "commit = \"$EXPECTED_GRAMMAR_COMMIT\"" extension.toml
-rg -Fq 'correct `commit`' README.md
-rg -Fq '"case" @keyword.conditional' languages/marmoset/highlights.scm
-rg -Fq '"override" @keyword.modifier' languages/marmoset/highlights.scm
-rg -Fq '"=>" @operator' languages/marmoset/highlights.scm
-rg -Fq '"&" @operator' languages/marmoset/highlights.scm
-rg -Fq '"%" @operator' languages/marmoset/highlights.scm
-rg -Fq '(fn_declaration' languages/marmoset/highlights.scm
-rg -Fq '(lambda_expression' languages/marmoset/highlights.scm
-rg -Fq '(derive_clause' languages/marmoset/highlights.scm
-rg -Fq '(fn_declaration' languages/marmoset/outline.scm
-rg -Fq 'target:' languages/marmoset/outline.scm
+search_fixed() {
+  local needle="$1"
+  local file="$2"
+  if command -v rg >/dev/null 2>&1; then
+    rg -Fq -- "$needle" "$file"
+  else
+    grep -Fq -- "$needle" "$file"
+  fi
+}
+
+search_fixed "commit = \"$EXPECTED_GRAMMAR_COMMIT\"" extension.toml
+search_fixed 'correct `commit`' README.md
+search_fixed '"case" @keyword.conditional' languages/marmoset/highlights.scm
+search_fixed '"override" @keyword.modifier' languages/marmoset/highlights.scm
+search_fixed '"=>" @operator' languages/marmoset/highlights.scm
+search_fixed '"&" @operator' languages/marmoset/highlights.scm
+search_fixed '"%" @operator' languages/marmoset/highlights.scm
+search_fixed '(fn_declaration' languages/marmoset/highlights.scm
+search_fixed '(lambda_expression' languages/marmoset/highlights.scm
+search_fixed '(derive_clause' languages/marmoset/highlights.scm
+search_fixed '(fn_declaration' languages/marmoset/outline.scm
+search_fixed 'target:' languages/marmoset/outline.scm
